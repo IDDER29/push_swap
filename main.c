872d54d07f119fa4ee9ecf6489d21ea750facaa2
Idder Ait El Mouden <139082259+IDDER29@users.noreply.github.com>
2026-01-07 04:05:06 +0100

    Implement move execution and cost-based sorting in main
    Added execute_moves and find_min_cost functions to perform stack operations based on calculated costs. Updated main loop to repeatedly move the lowest-cost node from stack_b to stack_a and finalize stack_a order, improving the sorting algorithm's efficiency.

--- main.c
+++ main.c
@@ -2,13 +2,55 @@
 #include "push_swap.h"
 #include "ft_printf.h"
 
+void execute_moves(t_list *stack_a, t_list *stack_b, t_node *node)
+{
+	int i;
+
+	i = 0;
+	while (i++ < node->rr_count)
+		rr(stack_a, stack_b);
+	i = 0;
+	while (i++ < node->rrr_count)
+		rrr(stack_a, stack_b);
+	i = 0;
+	while (i++ < node->ra_count)
+		ra(stack_a);
+	i = 0;
+	while (i++ < node->rb_count)
+		rb(stack_b);
+	i = 0;
+	while (i++ < node->rra_count)
+		rra(stack_a);
+	i = 0;
+	while (i++ < node->rrb_count)
+		rrb(stack_b);
+	pa(stack_a, stack_b);
+}
+
+t_node *find_min_cost(t_list *stack)
+{
+	t_node *current;
+	t_node *min_node;
+
+	if (!stack || !stack->head)
+		return (NULL);
+	min_node = stack->head;
+	current = stack->head->next;
+	while (current)
+	{
+		if (current->cost < min_node->cost)
+			min_node = current;
+		current = current->next;
+	}
+	return (min_node);
+}
 int main(void)
 {
-	int		a[] = {12, 67, 3, 0, -5, 99, 8, 23, -1, 42};
-	int		size_a;
-	int		i;
-	t_list	stack_a;
-	t_list	stack_b;
+	int a[] = {12, 67, 3, 0, -5, 99, 8, 23, -1, 42};
+	int size_a;
+	int i;
+	t_list stack_a;
+	t_list stack_b;
 
 	size_a = sizeof(a) / sizeof(a[0]);
 	i = 0;
@@ -72,6 +114,32 @@ int main(void)
 	print_stack(&stack_a);
 	ft_printf("STACK B :\n");
 	print_stack(&stack_b);
+
+	t_node *cheapest;
+	t_node *min_node;
+
+	while (stack_b.size > 0)
+	{
+		assign_indices(&stack_a);
+		assign_indices(&stack_b);
+		assign_targets(&stack_b, &stack_a);
+		calculate_cost(&stack_b, &stack_a);
+		cheapest = find_min_cost(&stack_b);
+		execute_moves(&stack_a, &stack_b, cheapest);
+	}
+	assign_indices(&stack_a);
+	min_node = find_index_zero_node(&stack_a);
+	if (min_node)
+	{
+		if (min_node->index <= stack_a.size / 2)
+			while (stack_a.head != min_node)
+				ra(&stack_a);
+		else
+			while (stack_a.head != min_node)
+				rra(&stack_a);
+	}
+	ft_printf("\nFinal sorted stack_a:\n");
+	print_stack(&stack_a);
 
 	stack_free(&stack_a);
 	stack_free(&stack_b);
